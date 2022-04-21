cb5b5a64e42fd678a1dce86889a1c00306914cc2
kasthuridinesh <kasthuridinesh153@gmail.com>
2022-04-21 23:11:56 +0530

    First commit

---
+++ Hello World/src/variables.java
@@ -0,0 +1,40 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+////if
+		int mark= 60;
+				if(mark> 50) {
+					System.out.println("qualified");
+				}
+					else {
+						System.out.println("not qualified");
+					
+					}
+//////if else
+				int marks = 50;
+				if (marks>50) {
+					System.out.println("qualified");
+				}
+				else if (marks== 50){
+					System.out.println("on hold");
+				}
+				else {
+					System.out.println("not qualifed");	
+				}
+/////////////////////////////
+				for(int d = 40; d>=10;d--){
+				System.out.println("descending numbers:"+d);
+				}
+////////////////////////////////////////////				
+				for(int a = 10; a<=40;a++){
+					System.out.println("Ascenting order:"+a);
+				}
+
+				}
+	}
+
+
